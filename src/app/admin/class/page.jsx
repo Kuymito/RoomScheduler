f65ae772e0f5a3cf650163c8f361d957ff42a2be
Seanghai123 <hseang815@gmail.com>
2025-06-12 08:46:26 +0700

    Edit class

--- src/app/admin/class/page.jsx
+++ src/app/admin/class/page.jsx
@@ -9,16 +9,16 @@ import ClassPageSkeleton from './components/ClassPageSkeleton';
 const fetchClassData = async () => {
     const initialClassData = [
         // Data is now inside the "API" function
-        { id: 1, name: 'NUM30-01', generation: '30', group: '01', major: 'IT', degrees: 'Bachelor', faculty: 'Faculty of IT', semester: 'Semester 1', shift: '7:00 - 10:00', status: 'active' },
-        { id: 2, name: 'NUM30-01', generation: '30', group: '01', major: 'IT', degrees: 'Bachelor', faculty: 'Faculty of IT', semester: 'Semester 1', shift: '7:00 - 10:00', status: 'active' },
-        { id: 3, name: 'NUM30-02', generation: '30', group: '02', major: 'CS', degrees: 'Bachelor', faculty: 'Faculty of CS', semester: 'Semester 1', shift: '8:00 - 11:00', status: 'active' },
-        { id: 4, name: 'NUM32-03', generation: '32', group: '03', major: 'IS', degrees: 'Bachelor', faculty: 'Faculty of IS', semester: 'Semester 2', shift: '9:00 - 12:00', status: 'active' },
-        { id: 5, name: 'NUM32-04', generation: '32', group: '04', major: 'SE', degrees: 'Bachelor', faculty: 'Faculty of SE', semester: 'Semester 2', shift: '13:00 - 16:00', status: 'active' },
-        { id: 6, name: 'NUM32-05', generation: '32', group: '05', major: 'AI', degrees: 'Bachelor', faculty: 'Faculty of AI', semester: 'Semester 2', shift: '15:00 PM - 18:00', status: 'active' },
-        { id: 7, name: 'NUM33-06', generation: '33', group: '06', major: 'DS', degrees: 'Bachelor', faculty: 'Faculty of DS', semester: 'Semester 3', shift: '17:00 - 20:00', status: 'active' },
-        { id: 8, name: 'NUM33-07', generation: '33', group: '07', major: 'ML', degrees: 'Bachelor', faculty: 'Faculty of ML', semester: '2Semester 3', shift: '18:00 - 21:00', status: 'active' },
-        { id: 9, name: 'NUM33-08', generation: '33', group: '08', major: 'DA', degrees: 'Bachelor', faculty: 'Faculty of DA', semester: 'Semester 3', shift: '19:00 - 22:00', status: 'archived' },
-        { id: 10, name: 'NUM33-09', generation: '33', group: '09', major: 'SE', degrees: 'Bachelor', faculty: 'Faculty of SE & R', semester: '2024-2025 S3', shift: '8:00 - 11:00', status: 'active' }
+        { id: 1, name: 'NUM30-45', generation: '30', group: '45', major: 'IT', degrees: 'PH.D', faculty: 'NUM-FIT', semester: 'Semester 1', shift: '7:00 - 10:00', status: 'active' },
+        { id: 2, name: 'NUM30-41', generation: '30', group: '41', major: 'English', degrees: 'M.A', faculty: 'NUM-FON', semester: 'Semester 1', shift: '7:00 - 10:00', status: 'active' },
+        { id: 3, name: 'NUM30-15', generation: '30', group: '15', major: 'BIT', degrees: 'M.A', faculty: 'NUM-FOA', semester: 'Semester 1', shift: '8:00 - 11:00', status: 'active' },
+        { id: 4, name: 'NUM32-75', generation: '32', group: '75', major: 'Law', degrees: 'PH.D', faculty: 'NUM-FIT', semester: 'Semester 2', shift: '9:00 - 12:00', status: 'active' },
+        { id: 5, name: 'NUM32-28', generation: '32', group: '28', major: 'Management', degrees: 'M.A', faculty: 'NUM-FOM', semester: 'Semester 2', shift: '13:00 - 16:00', status: 'active' },
+        { id: 6, name: 'NUM32-05', generation: '32', group: '05', major: 'Touriusm', degrees: 'PH.D', faculty: 'NUM-FOE', semester: 'Semester 2', shift: '15:00 PM - 18:00', status: 'active' },
+        { id: 7, name: 'NUM33-14', generation: '33', group: '14', major: 'IT', degrees: 'PH.D', faculty: 'NUM-FBE', semester: 'Semester 3', shift: '17:00 - 20:00', status: 'active' },
+        { id: 8, name: 'NUM34-17', generation: '34', group: '17', major: 'Management', degrees: 'M.A', faculty: 'NUM-FON', semester: '2Semester 3', shift: '18:00 - 21:00', status: 'active' },
+        { id: 9, name: 'NUM33-75', generation: '33', group: '75', major: 'Finance', degrees: 'M.A', faculty: 'NUM-FIT', semester: 'Semester 3', shift: '19:00 - 22:00', status: 'archived' },
+        { id: 10, name: 'NUM33-11', generation: '33', group: '11', major: 'IT', degrees: 'PH.D', faculty: 'NUM-FOT', semester: '2024-2025 S3', shift: '8:00 - 11:00', status: 'active' }
     ];
     // Simulate a network delay of 1 second
     return new Promise(resolve => setTimeout(() => resolve(initialClassData), 1000));
